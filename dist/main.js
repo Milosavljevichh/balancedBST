014315af029f60aee28872d34f18c13adc11aa2a
Miloš Milosavljević <112649014+Milosavljevichh@users.noreply.github.com>
2024-04-07 12:21:21 +0200

    Initial commit

---
+++ dist/main.js
@@ -0,0 +1,6 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+console.log('works');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOztBQUFBLHFCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcHJvamVjdC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnd29ya3MnKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
